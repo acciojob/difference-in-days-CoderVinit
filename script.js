5cfc390bcf35ae5d144ad8f770a726236d472ab3
VINIT KUMAR PATEL <101181057+CoderVinit@users.noreply.github.com>
2025-03-22 10:09:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	 let d1 = new Date(date1);
+    let d2 = new Date(date2);
+
+    // Calculate the difference in milliseconds
+    let diffTime = d2 - d1;
+
+    // Convert milliseconds to days
+    let diffDays = diffTime / (1000 * 60 * 60 * 24);
+
+    return diffDays;
 };
 
 // Do not change the code below.
